b11b31e8ee07e82d158937251e7ed1f8dbc572d1
Marcos Benatti Lauer <benatti77@gmail.com>
2023-02-07 16:54:13 -0300

    test: test if white/black can attack each other providing other pieces list

--- test/queens_test.exs
+++ test/queens_test.exs
@@ -18,4 +18,30 @@ defmodule QueensTest do
       refute Queens.can_attack?({3, 3}, {6, 5})
     end
   end
+
+  describe "can_attack?/3" do
+    test "if the other_pieces_positions on same row" do
+      assert Queens.can_attack?({2, 1}, {2, 5}, [{2, 0}])
+    end
+
+    test "if the other_pieces_positions on same column" do
+      assert Queens.can_attack?({1, 0}, {3, 0}, [{0, 0}])
+    end
+
+    test "if the other_pieces_positions on diagonal" do
+      assert Queens.can_attack?({1, 1}, {3, 3}, [{4, 4}])
+    end
+
+    test "cannot attack if the other_pieces_positions on same row but in the middle" do
+      refute Queens.can_attack?({0, 1}, {0, 6}, [{0, 3}])
+    end
+
+    test "cannot attack if the other_pieces_positions on same column but in the middle" do
+      refute Queens.can_attack?({1, 0}, {3, 0}, [{2, 0}])
+    end
+
+    test "cannot attack if the other_pieces_positions on diagonal but in the middle" do
+      refute Queens.can_attack?({1, 1}, {3, 3}, [{2, 2}])
+    end
+  end
 end
